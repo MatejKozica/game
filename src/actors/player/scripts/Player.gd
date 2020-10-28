3b2bc3167db9ba3d50791c1703370fd8989b0fd6
Matej Kozica <matej.split@gmail.com>
2020-10-28 16:02:17 +0100

    Add attack animation

--- src/actors/player/scripts/Player.gd
+++ src/actors/player/scripts/Player.gd
@@ -5,7 +5,7 @@ onready var animation_tree = $AnimationTree;
 onready var animation_state = animation_tree.get("parameters/playback");
 
 enum{
-	IDLE, WALK
+	IDLE, WALK, ATTACK
 }
 
 var state = IDLE;
@@ -29,22 +29,40 @@ func _state_machine() -> void:
 			#checking for other states
 			if _direction != Vector2.ZERO:
 				state = WALK;
+			if Input.is_action_just_pressed("attack"):
+				state = ATTACK;
+				print("udri");
 		WALK:
 			#state
 			_direction = _get_direction();
 			_velocity = move(_velocity, _direction, move_speed);
 			animation_state.travel("walk");
+			
 			#checking for other states
 			if _velocity == Vector2.ZERO:
 				state = IDLE;
+			if Input.is_action_just_pressed("attack"):
+				state = ATTACK;
+				print("udri");
+		ATTACK:
+			#state
+#			_direction = _get_direction();
+			animation_state.travel("attack_sword");
+			
+			#checking for other states
+			
 
 func _get_direction() -> Vector2:
 	var direction = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),
 							Input.get_action_strength("down") - Input.get_action_strength("up"));
 	animation_tree.set("parameters/walk/blend_position", _direction);
 	animation_tree.set("parameters/idle/blend_position", _direction);
+	animation_tree.set("parameters/attack_sword/blend_position", _direction);
 	return direction;
 
 func move(velocity: Vector2, direction: Vector2, speed) -> Vector2:
 	velocity = speed * direction.normalized();
 	return velocity;
+	
+func setIdle() -> void:
+	state = IDLE;
