1428884b8043fbd6637309e957bee1dd043af53c
Matej Kozica <matej.split@gmail.com>
2020-11-30 15:43:12 +0100

    Change assets and add crosshair

--- src/actors/actor.gd
+++ src/actors/actor.gd
@@ -6,5 +6,5 @@ class_name Actor
 export var _velocity: = Vector2.ZERO;
 export var _direction: = Vector2(0,1);
 
-export var move_speed: = 4 * 32;
+export var move_speed: = 10 * 32;
 
--- src/actors/player/scripts/Player.gd
+++ src/actors/player/scripts/Player.gd
@@ -1,17 +1,14 @@
 extends "res://src/actors/actor.gd";
 
 onready var sprite = $Sprite;
-onready var animation_tree = $AnimationTree;
+onready var animation_tree  = $AnimationTree;
 onready var animation_state = animation_tree.get("parameters/playback");
 
 enum{
-	IDLE, WALK, ATTACK
+	IDLE, WALK, SHOOT
 }
 
 var state = IDLE;
-
-func _ready():
-	animation_tree.active = true;
 
 func _physics_process(delta):
 	_state_machine();
@@ -26,40 +23,36 @@ func _state_machine() -> void:
 			_direction = _get_direction();
 			animation_state.travel("idle");
 			
+			
 			#checking for other states
 			if _direction != Vector2.ZERO:
 				state = WALK;
 			if Input.is_action_just_pressed("attack"):
-				state = ATTACK;
-				print("udri");
+				state = SHOOT;
 		WALK:
 			#state
 			_direction = _get_direction();
 			_velocity = move(_velocity, _direction, move_speed);
-			animation_state.travel("walk");
 			
 			#checking for other states
 			if _velocity == Vector2.ZERO:
 				state = IDLE;
 			if Input.is_action_just_pressed("attack"):
-				state = ATTACK;
-				print("udri");
-		ATTACK:
+				state = SHOOT;
+		SHOOT:
 			#state
 			_direction = _get_direction();
-			animation_state.travel("attack_sword");
+			animation_state.travel("shooting");
 
 func _get_direction() -> Vector2:
 	var direction = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),
 							Input.get_action_strength("down") - Input.get_action_strength("up"));
-	animation_tree.set("parameters/walk/blend_position", _direction);
-	animation_tree.set("parameters/idle/blend_position", _direction);
-	animation_tree.set("parameters/attack_sword/blend_position", _direction);
+	look_at(get_global_mouse_position());
 	return direction;
 
 func move(velocity: Vector2, direction: Vector2, speed) -> Vector2:
 	velocity = speed * direction.normalized();
 	return velocity;
-	
-func setIdle() -> void:
+
+func set_idle():
 	state = IDLE;
